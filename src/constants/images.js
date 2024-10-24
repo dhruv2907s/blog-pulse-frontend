ac094a067b8a668f065fd1e594aa4cee7edb293e
aviraj09 <aviraj0902@gmail.com>
2024-10-24 13:56:13 +0530

    a

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,8 @@ import Logo from "../assets/Logo.png";
 import blog_pulse_logo from "../assets/blog_pulse_logo.svg"
 import HomePageDemo from "../assets/HomePageDemo.svg";
 import post1 from "../assets/Posts/post1.jpg";
-import CTA from "../assets/CTA.png";
+import CTA from "../assets/CTA.avif";
+import blog_pulse_logo_black from "../assets/blog_pulse_logo_black.svg";
 const samplePostImage = "/images/sample.png";
 const userImage = "/images/user.png";
 const images = {
@@ -12,7 +13,8 @@ const images = {
     CTA,
     samplePostImage,
     userImage,
-    blog_pulse_logo
+    blog_pulse_logo,
+    blog_pulse_logo_black
 };
 
 export default images;
