69b05885544a6418568e8975415d113b6c00dd9c
aviraj09 <aviraj0902@gmail.com>
2024-11-11 14:52:09 +0530

    color change

--- src/pages/container/Hero.jsx
+++ src/pages/container/Hero.jsx
@@ -4,7 +4,7 @@ import { images } from "../../constants";
 
 const Hero = () => {
     return (
-        <section className="bg-gradient-to-br from-green-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
+        <section className="bg-gradient-to-br from-purple-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center">
                 <div className="lg:w-1/2 lg:pr-12">
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-white text-center lg:text-left mb-6">
